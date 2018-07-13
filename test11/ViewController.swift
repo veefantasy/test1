db76236f452db69855414e517a2de9cc41ab2a10
Aravindhan <aravind@aravindhans-macbook-pro.local>
2018-07-14 00:26:57 +0530

    zx

---
+++ test11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test11
+//
+//  Created by Aravindhan on 14/07/18.
+//  Copyright © 2018 XING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
